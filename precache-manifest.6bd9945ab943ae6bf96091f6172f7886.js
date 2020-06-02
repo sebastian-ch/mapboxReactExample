ec860079b937cd6c5e03f59f11959e19403ddb21
Sebastian Hancock <sebastian.hancock@tum.de>
2020-06-02 15:31:08 +0200

    Updates

---
+++ precache-manifest.6bd9945ab943ae6bf96091f6172f7886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4fae562f6d385f19587b75b6fe3080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ded952d762ac86938ec",
+    "url": "/static/css/main.c252fddb.chunk.css"
+  },
+  {
+    "revision": "f92d65f067fce3934fca",
+    "url": "/static/js/2.68eea59d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68eea59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ded952d762ac86938ec",
+    "url": "/static/js/main.f4b98b50.chunk.js"
+  },
+  {
+    "revision": "b3c6ac6c746954674575",
+    "url": "/static/js/runtime-main.d606ac59.js"
+  }
+]);
