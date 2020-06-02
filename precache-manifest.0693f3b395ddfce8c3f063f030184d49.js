71dd3aef02dc5f3e974276ddedbbdccb31649924
Sebastian Hancock <sebastian.hancock@tum.de>
2020-06-02 15:37:37 +0200

    Updates

---
+++ precache-manifest.0693f3b395ddfce8c3f063f030184d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253d54d60f0a88c8d53910f22b4daab3",
+    "url": "/mapboxReactExample/index.html"
+  },
+  {
+    "revision": "6ded952d762ac86938ec",
+    "url": "/mapboxReactExample/static/css/main.c252fddb.chunk.css"
+  },
+  {
+    "revision": "f92d65f067fce3934fca",
+    "url": "/mapboxReactExample/static/js/2.68eea59d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mapboxReactExample/static/js/2.68eea59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ded952d762ac86938ec",
+    "url": "/mapboxReactExample/static/js/main.f4b98b50.chunk.js"
+  },
+  {
+    "revision": "b374eaf45250383ffe95",
+    "url": "/mapboxReactExample/static/js/runtime-main.78c68941.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bd9945ab943ae6bf96091f6172f7886.js"
+  "/mapboxReactExample/precache-manifest.0693f3b395ddfce8c3f063f030184d49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mapboxReactExample/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
